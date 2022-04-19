eb8768230be101ac8603347955c7318ea602b261
Ezra Szanton <54345209+Delverino@users.noreply.github.com>
2022-04-19 11:22:38 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 var http = require('http');
-
+var port = process.env.PORT || 3000
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("Hello World");
   res.end();
-}).listen(8080);
+}).listen(port);
